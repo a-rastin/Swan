0c3ce71a65950a1418b041637631f2037ddab970
a-rastin <arastin.md@gmail.com>
2026-06-16 19:55:51 -0700

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
   plugins: [
